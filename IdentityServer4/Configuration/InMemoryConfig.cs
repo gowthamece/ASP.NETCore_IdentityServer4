f7c19a0f9c55e643d0fac551e49e95df70f44e1e
Gowtham K <gowthamk91@outlook.com>
2022-08-06 13:51:25 +0530

    Add project files.

---
+++ IdentityServer4/Configuration/InMemoryConfig.cs
@@ -0,0 +1,56 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityServer4Sample.Configuration
+{
+    public static class InMemoryConfig
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources() =>
+      new List<IdentityResource>
+      {
+          new IdentityResources.OpenId(),
+          new IdentityResources.Profile()
+      };
+
+        public static List<TestUser> GetUsers() =>
+    new List<TestUser>
+    {
+      new TestUser
+      {
+          SubjectId = "a9ea0f25-b964-409f-bcce-c923266249b4",
+          Username = "Gowtham",
+          Password = "GowthamPassword",
+          Claims = new List<Claim>
+          {
+              new Claim("given_name", "Gowtham"),
+              new Claim("family_name", "Kumar")
+          }
+      },
+      new TestUser
+      {
+          SubjectId = "c95ddb8c-79ec-488a-a485-fe57a1462340",
+          Username = "Shaaniya",
+          Password = "ShaaniyaPassword",
+          Claims = new List<Claim>
+          {
+              new Claim("given_name", "Shaaniya"),
+              new Claim("family_name", "Gowtham")
+          }
+      }
+    };
+        public static IEnumerable<Client> GetClients() =>
+    new List<Client>
+    {
+       new Client
+       {
+            ClientId = "identityClientid",
+            ClientSecrets = new [] { new Secret("identityClientsecret".Sha512()) },
+            AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+            AllowedScopes = { IdentityServerConstants.StandardScopes.OpenId }
+        }
+    };
+    }
+}
---
+++ IdentityServer4/Startup.cs
@@ -0,0 +1,46 @@
+using IdentityServer4Sample.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer4
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+        .AddTestUsers(InMemoryConfig.GetUsers())
+        .AddInMemoryClients(InMemoryConfig.GetClients())
+        .AddDeveloperSigningCredential(); //not something we want to use in a production environment;;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
